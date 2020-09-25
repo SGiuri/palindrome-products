e9a1ef6c27dac32c7b9a9babcf8aeac0e9c50a56
s.giuri <s.giuri@issm.it>
2020-09-25 14:16:02 +0200

    WIP2

--- palindrome_products.py
+++ palindrome_products.py
@@ -1,16 +1,21 @@
 def largest(min_factor, max_factor):
 
-    value = max_factor * max_factor
+    first_factor = max_factor
+    second_factor = max_factor
+    value = first_factor * second_factor
+
     found = False
     factors = []
-    if is_palindrome(value):
-        found_factors(value, max_factor, min_factor )
+
     for first_factor in range(max_factor,min_factor-1,-1):
         for second_factor in range(max_factor, min_factor - 1, -1):
-            value I first_factor*second_factor
+            value = first_factor * second_factor
             if is_palindrome(value):
-                found = True
-
+                found == True
+                break
+        if found:
+            break
+    print(value)
 
 
 
@@ -27,4 +32,6 @@ def is_palindrome(number):
     return string_number == string_number[::-1]
 
 def find_factors(value, min_factor, max_factor):
+    pass
 
+largest(100,999)
