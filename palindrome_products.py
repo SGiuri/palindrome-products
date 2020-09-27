11fe08d93754ce31c4efa7611fbf28f5e2f0d7c4
SGiuri <s.giuri@issm.it>
2020-09-27 21:42:59 +0200

    V4

--- palindrome_products.py
+++ palindrome_products.py
@@ -7,6 +7,24 @@ def largest(min_factor, max_factor):
     max_value = max_factor*max_factor
     min_value = min_factor*min_factor
     palindrome = []
+    for first_factor in range(max_factor, min_factor-1, -1):
+        for second_factor in range(max_factor, first_factor-1, -1):
+            value = first_factor * second_factor
+
+            print(first_factor, second_factor, value)
+            if is_palindrome(value):
+                return value, find_factors(value, min_factor, max_factor)
+
+
+def smallest(min_factor, max_factor):
+
+    first_factor = min_factor
+    second_factor = min_factor
+    value = first_factor * second_factor
+
+    max_value = max_factor*max_factor
+    min_value = min_factor*min_factor
+    palindrome = []
     for first_factor in range(max_factor, min_factor, -1):
         for second_factor in range(max_factor, first_factor, -1):
             value = first_factor * second_factor
@@ -16,13 +34,6 @@ def largest(min_factor, max_factor):
     factors = find_factors(max_value, min_factor, max_factor)
 
     return max_value,factors
-
-    pass
-
-
-def smallest(min_factor, max_factor):
-    pass
-
 
 def is_palindrome(number):
     string_number = str(number)
@@ -34,7 +45,7 @@ def find_factors(value, min_factor, max_factor):
     factors = []
     for j in range(min_factor, max_factor):
         if value % j == 0:
-            print(value, j, value//j)
+            #print(value, j, value//j)
             if min_factor < (value // j) < max_factor:
                 if sorted([j, value // j]) not in factors:
                     factors.append(sorted([j, value // j]))
