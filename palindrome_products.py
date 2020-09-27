51dc67523f3f01f15e5c7bac765c8e0a8599bede
SGiuri <s.giuri@issm.it>
2020-09-27 15:58:36 +0200

    PReparing for big modifications

--- palindrome_products.py
+++ palindrome_products.py
@@ -1,5 +1,4 @@
 def largest(min_factor, max_factor):
-
     first_factor = max_factor
     second_factor = max_factor
     value = first_factor * second_factor
@@ -7,17 +6,16 @@ def largest(min_factor, max_factor):
     found = False
     factors = []
 
-    for first_factor in range(max_factor,min_factor-1,-1):
+    for first_factor in range(max_factor, min_factor - 1, -1):
         for second_factor in range(max_factor, min_factor - 1, -1):
             value = first_factor * second_factor
-            if is_palindrome(value):
-                found == True
+            found = is_palindrome(value)
+            if found:
                 break
         if found:
             break
-    print(value)
-
-
+    factors = find_factors(value, min_factor, max_factor)
+    print(value, factors)
 
     pass
 
@@ -31,7 +29,17 @@ def is_palindrome(number):
 
     return string_number == string_number[::-1]
 
-def find_factors(value, min_factor, max_factor):
-    pass
 
-largest(100,999)
+def find_factors(value, min_factor, max_factor):
+    factors = []
+    for j in range(min_factor, max_factor):
+        if value % j == 0:
+            print(value, j, value//j)
+            if min_factor < (value // j) < max_factor:
+                factors.append([j, value // j])
+    return factors
+
+
+#largest(100, 999)
+
+print(find_factors(580085, 100, 999))
